51303ce947d604271a7a2b70a10e40821bcb50fa
DeniRadionov <108937144+DeniRadionov@users.noreply.github.com>
2023-10-12 11:14:00 +0300

    data base

---
+++ Web-scrapping/gasscrapping/gasscrapping/database/1_json_processing.py
@@ -0,0 +1,18 @@
+import json
+path = '../spiders/gasPrices.json'
+with open(path, 'r', encoding='utf-8') as input_file:
+    data = json.load(input_file)
+
+
+new_data = [entry for entry in data if entry.get('address') is not None]
+
+
+for entry in new_data:
+    if entry.get('station-name') is None:
+        entry['station-name'] = 'ЗАПРАВКА'
+
+with open('output.json', 'w', encoding='utf-8') as output_file:
+    json.dump(new_data, output_file, indent=4, ensure_ascii=False)
+
+input_file.close()
+output_file.close()
