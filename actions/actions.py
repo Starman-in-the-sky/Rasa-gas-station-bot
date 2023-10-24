77bd22c4e02f3b4f9c9d936373045aa11693e3ec
starman-in-the-sky <ms.practice@yandex.ru>
2023-10-25 00:22:15 +0300

    I'm so tired

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,25 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import requests
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
 
+class ActionCreateRoute(Action):
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def name(self) -> Text:  # регистрируем имя действия
+        return "action_create_route"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher, tracker, domain):
+        from_place = "Химки, Московская область"
+        to_place = tracker.get_slot('station_company_name')
+
+        # Отправка запроса к API Yandex Maps для получения маршрута
+        response = requests.get(
+            f"https://api-maps.yandex.ru/2.1/?apikey=5941ca0f-88a5-462a-b68b-049e98bd248c&lang=ru_RU&mode=routes&from={from_place}&to={to_place}")
+        route = response.json()['response']['route'][0]['steps']
+
+        # Отправка ответа пользователю
+        route_info = ""
+        for step in route:
+            route_info += step['instruction'] + "\n"
+        dispatcher.utter_message(template="utter_route", text=f"Маршрут от Химок до {to_place}:\n{route_info}")
+
+        return route_info
