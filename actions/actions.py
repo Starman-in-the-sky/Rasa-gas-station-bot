0deb8862662c7dc9e9378b307664204091962cac
starman-in-the-sky <ms.practice@yandex.ru>
2023-11-26 01:08:21 +0300

    version_6 working

--- actions/actions.py
+++ actions/actions.py
@@ -1,4 +1,5 @@
 import json
+from math import sqrt
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -42,7 +43,7 @@ class ActionParseUserAddress(Action):
             # client = Client("6e08bccf-7da4-46e3-9623-a5ddfd850be6")
             # coordinates = client.coordinates(uaddress)
 
-            SlotSet("user_coordinates", "55.838369,37.483113")
+            SlotSet("user_coordinates", "Ленинградское шоссе, 56, Москва, 125212")
 
             dispatcher.utter_message(text=f"Благодарю! Теперь вы можете посмотреть адреса ближайших заправок, проложить маршрут, узнать цены на бензин и т.д. Что бы вы хотели сделать?")
         except Exception as inst:
@@ -83,6 +84,46 @@ class ActionGetPrices(Action):
         return []
 
 
+class ActionGetNearestStations(Action):
+    def name(self) -> Text:
+        return "check_nearest_stations"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        def calculate_distance(loc1, loc2):
+            return sqrt((loc1[0] - loc2[0]) ** 2 + (loc1[1] - loc2[1]) ** 2)
+        try:
+            path = 'E:\Rasa-bot\Rasa-gas-station-bot\output.json'
+            user_location = (37.483113, 55.838369)
+            with open(path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+
+            for station in data:
+                station['distance'] = calculate_distance(user_location, (station['x'], station['y']))
+
+            # Сортировка заправок по расстоянию
+            data.sort(key=lambda x: x['distance'])
+
+            # Выбор трёх ближайших станций
+            closest_stations = data[:3]
+
+            # Формирование итогового массива
+            result = [{'station-name': station['station-name'], 'address': station['address']} for station in
+                      closest_stations]
+
+            dispatcher.utter_message(text=f"Ближайшие заправки: {result[0]['station-name']} по адресу - {result[0]['address']}, {result[1]['station-name']} по адресу - {result[1]['address']}, {result[2]['station-name']} по адресу - {result[2]['address']}")
+
+        except Exception as inst:
+            dispatcher.utter_message(text=f"Произошла ошибка при работе бота  Тип исключения: {type(inst)}  Аргументы исключения: {inst.args}  Исключение: {inst}")
+            print(f"Тип исключения: {type(inst)}")
+            print(f"Аргументы исключения: {inst.args}")
+            print(f"Исключение: {inst}")
+
+        return []
+
+
 class ActionCreateRoute(Action):
 
     def name(self) -> Text:
@@ -93,7 +134,7 @@ class ActionCreateRoute(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            from_place = tracker.get_slot("user_coordinates")
+            from_place = tracker.get_slot("user_address")
             to_place = tracker.get_slot("station_company_name")
 
             # Создаем URL для маршрута на Яндекс Картах
